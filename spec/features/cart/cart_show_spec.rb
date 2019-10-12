4fee4db9d3faf90ab2bd57404ddbc0b58d6aa188
Hillary Stewart <hillarystewart4427@gmail.com>
2019-10-12 11:39:49 -0600

    Add spec for checkout link
    Co-Authored-By: David Lopez <Not-Zorro@users.noreply.github.com>

--- spec/features/cart/cart_show_spec.rb
+++ spec/features/cart/cart_show_spec.rb
@@ -167,4 +167,38 @@ describe "Cart Index" do
     end
     expect(page).to_not have_css("#item-cart-#{dog_bone.id}")
   end
+
+  it "can link to checkout page" do
+    meg = Merchant.create(name: "Meg's Bike Shop", address: '123 Bike Rd.', city: 'Denver', state: 'CO', zip: 80203)
+    brian = Merchant.create(name: "Brian's Dog Shop", address: '125 Doggo St.', city: 'Denver', state: 'CO', zip: 80210)
+
+    tire = meg.items.create(name: "Gatorskins", description: "They'll never pop!", price: 100, image: "https://www.rei.com/media/4e1f5b05-27ef-4267-bb9a-14e35935f218?size=784x588", inventory: 12)
+
+    pull_toy = brian.items.create(name: "Pull Toy", description: "Great pull toy!", price: 10, image: "http://lovencaretoys.com/image/cache/dog/tug-toy-dog-pull-9010_2-800x800.jpg", inventory: 32)
+    dog_bone = brian.items.create(name: "Dog Bone", description: "They'll love it!", price: 20, image: "https://img.chewy.com/is/image/catalog/54226_MAIN._AC_SL1500_V1534449573_.jpg", inventory: 21)
+
+    visit '/items'
+
+    within "#item-#{pull_toy.id}" do
+      click_button 'Add to cart'
+    end
+
+    within "#item-#{dog_bone.id}" do
+      click_button 'Add to cart'
+      click_button 'Add to cart'
+    end
+
+    visit '/cart'
+
+    click_link "Proceed to checkout"
+
+    expect(current_path).to eq('/cart/checkout')
+  end
+  # As a visitor
+  # When I have items in my cart
+  # And I visit my cart
+  # I see a button or link to Checkout
+  # When I click that button, I am taken to the new order page
+
+
 end
