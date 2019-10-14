f7408bfad77e52a637dab081012bad1bfffcb3d4
David Lopez <ldavid.x.lopezl@gmail.com>
2019-10-14 11:50:38 -0600

    Add flash message for order
    Co-Authored-By: Hillary <hillstew@users.noreply.github.com>

--- app/controllers/order_controller.rb
+++ app/controllers/order_controller.rb
@@ -5,30 +5,35 @@ class OrderController < ApplicationController
     @items = cart.cart_items
   end
 
-  def create 
-    order = Order.create(
-      token: Order.token, 
-      grand_total: cart.grand_total
-    )
-    session[:customer] = customer_params
-    cart.cart_items.each do |cart_item, amount| 
-      order.item_orders.create(
-        quantity: amount,
-        order_id: order.id,
-        item_id: cart_item.id
+  def create
+    if !customer_params.values.include?("")
+      order = Order.create(
+        token: Order.token,
+        grand_total: cart.grand_total
       )
+      session[:customer] = customer_params
+      cart.cart_items.each do |cart_item, amount|
+        order.item_orders.create(
+          quantity: amount,
+          order_id: order.id,
+          item_id: cart_item.id
+        )
+      end
+        cart.empty_cart
+        session[:cart] = cart.contents
+        redirect_to "/order/#{order.id}"
+    else
+      flash[:error] = "Please complete all fields."
+      redirect_to "/cart/checkout"
     end
-    cart.empty_cart
-    session[:cart] = cart.contents
-    redirect_to "/order/#{order.id}"
   end
 
   def show
     @order = Order.find(params[:order_id])
   end
 
-  private 
-    def customer_params 
+  private
+    def customer_params
       params.permit(:name, :address, :city, :state, :zip)
     end
 end
