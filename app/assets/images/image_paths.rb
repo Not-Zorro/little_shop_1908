80e4922ee71dc049b2ee9fc5936aee30f9f71d7c
Hillary Stewart <hillarystewart4427@gmail.com>
2019-10-15 07:16:10 -0600

    Update seed data

--- app/assets/images/image_paths.rb
+++ app/assets/images/image_paths.rb
@@ -1,3 +1,16 @@
 @images = {
-  "1" => "https://cdni.llbean.net/is/image/wim/273137_7370_41?hei=302&wid=265"
+  "red_flannel" => "https://www.duluthtrading.com/dw/image/v2/BBNM_PRD/on/demandware.static/-/Sites-dtc-master-catalog/default/dw9c64d84d/images/large/89802_RYD.jpg?sw=600&sh=600&sm=fit",
+  "purple_flannel" => "https://www.duluthtrading.com/dw/image/v2/BBNM_PRD/on/demandware.static/-/Sites-dtc-master-catalog/default/dw3e01eced/images/large/89802_PRO.jpg?sw=600&sh=600&sm=fit",
+  "blue_flannel" => "https://www.duluthtrading.com/dw/image/v2/BBNM_PRD/on/demandware.static/-/Sites-dtc-master-catalog/default/dw5f688ed3/images/large/89802_ULP.jpg?sw=600&sh=600&sm=fit",
+  "bck_wht_flannel" => "https://www.duluthtrading.com/dw/image/v2/BBNM_PRD/on/demandware.static/-/Sites-dtc-master-catalog/default/dw64074925/images/large/89802_PHE.jpg?sw=600&sh=600&sm=fit",
+  "org_blue_flannel" => "https://www.duluthtrading.com/dw/image/v2/BBNM_PRD/on/demandware.static/-/Sites-dtc-master-catalog/default/dwada0d689/images/large/92326_RRD.jpg?sw=600&sh=600&sm=fit",
+  "blue_red_flannel" => "https://www.duluthtrading.com/dw/image/v2/BBNM_PRD/on/demandware.static/-/Sites-dtc-master-catalog/default/dwcb5b72c1/images/large/92326_BUX.jpg?sw=600&sh=600&sm=fit",
+  "brown_flannel" => "https://www.duluthtrading.com/dw/image/v2/BBNM_PRD/on/demandware.static/-/Sites-dtc-master-catalog/default/dw41694a6a/images/large/92326_SID.jpg?sw=600&sh=600&sm=fit",
+  "blue_hawaiian" => "https://cdn.shopify.com/s/files/1/0077/0432/products/short-sleeve-shirts-the-flower-the-leader-1_1024x1024.jpg?v=1547495935",
+  "blue_pink_hawaiian" => "https://cdn.shopify.com/s/files/1/0077/0432/products/HawaiianLion_POPOVER_LD_FRONT_1_1024x1024.jpg?v=1555970859",
+  "kahlua_hawaiian" => "https://cdn.shopify.com/s/files/1/0077/0432/products/Kahlua_Button-UpStretch__front_web_1024x1024.jpg?v=1565293801",
+  "flower_hawaiian" => "https://cdn.shopify.com/s/files/1/0077/0432/products/TheFloweryFlapper30179_web_1024x1024.jpg?v=1570473069",
+  "palm_tree_hawaiian" => "https://cdn.shopify.com/s/files/1/0077/0432/products/WTH_NUTTER_FRONTweb_f0a8c16d-a41c-4154-ae07-339f9f238496_1024x1024.jpg?v=1528301804",
+  "muted_hawaiian" => "https://cdn.shopify.com/s/files/1/0077/0432/products/Resort-Wear_31512_web_1024x1024.jpg?v=1569960329",
+  "cranberry_shirt" => "https://target.scene7.com/is/image/Target/GUEST_64475899-90ef-4794-b7e5-7804c9b15fdd?fmt=webp&wid=1400&qlt=80"
 }
